7f0c3badb7fe55132ce5f9eae5e5aff693be3941
jadhav0505 <119648693+jadhav0505@users.noreply.github.com>
2022-12-05 21:15:55 +0530

    addition of two number .c

---
+++ addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a=10,b=30;
+  clrscr();
+  printf("\n value of a=%d",a);
+  printf("\n value of b=%d",b);
+  
+  printf("\n addition=%d",a+b);
+ getch();
+}
