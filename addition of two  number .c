2451555605566983a8a9c5cb709e8cee67d536f8
jadhav0505 <119648693+jadhav0505@users.noreply.github.com>
2022-12-05 21:06:45 +0530

    addition take value for user

---
+++ addition of two  number .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr();
+  printf("\n enter a value of a=")
+  scanf("%d",&a);
+  printf("\n enter a value of b=")
+  scanf("%d",&b);
+  c=a+b;
+  printf("\n addition=%d",c);
+getch();
+}
