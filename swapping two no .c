04ac84e5d2e95b4e5113337609e082df3b117a25
jadhav0505 <119648693+jadhav0505@users.noreply.github.com>
2022-12-10 00:58:35 +0530

    swapping two numbers use third variable .c

---
+++ swapping two no .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr();
+  printf("\n Enter the two values=");
+  scanf("%d%d",&a,&b);
+  printf(\n Befor swapping:");
+  printf("\n a:%d\t b:%d",a,b);
+  c=a;
+  b=c;
+  a=b;
+  printf("\n After swapping :");
+  printf("\n a:%d\t b:%d",a.b);
+  getch();
+ }
