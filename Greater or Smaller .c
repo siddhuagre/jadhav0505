c12e8ecb328bfcf4475c50c78644c1eedb95b669
jadhav0505 <119648693+jadhav0505@users.noreply.github.com>
2022-12-10 22:21:04 +0530

    check the given no is greater or smaller .c

---
+++ Greater or Smaller .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b;
+  clrscr();
+  printf("\n Enter the no :");
+  scanf("%d%d",&a,&b);
+  if(a>b)
+  {
+    printf("\n The given no a is greater:");
+  }
+  else
+  {
+    printf("\n The give no a is smaller:");
+  }
+  getch();
+}
